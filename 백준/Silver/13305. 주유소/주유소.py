0b53825e66d9fe6bffcdfb29872c8420cbef9a1e
seon_ <108112575+seon03@users.noreply.github.com>
2024-05-12 15:07:06 +0900

    [Silver III] Title: 주유소, Time: 128 ms, Memory: 46236 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -5,8 +5,8 @@ road = list(map(int, input().split()))
 cost = list(map(int, input().split()))
 min = cost[0]*road[0]
 for i in range(1, len(road)):
-  if cost[i-1] < cost[i]:
-    min += cost[i-1] * road[i]
-  else:
-    min += cost[i] * road[i]
+  if cost[i] > cost[i-1]:
+    cost[i] = cost[i-1]
+  
+  min += cost[i]*road[i]
 print(min)
