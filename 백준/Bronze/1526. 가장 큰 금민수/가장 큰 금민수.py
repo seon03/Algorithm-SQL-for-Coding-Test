f72c6a57aa070ac21064aed370d73b22ba338655
서선아_T7343 <108112575+seon03@users.noreply.github.com>
2024-10-11 15:52:26 +0900

    [Bronze I] Title: 가장 큰 금민수, Time: 96 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1526. 가장 큰 금민수/가장 큰 금민수.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+while True:
+    flag = True
+    for i in str(n):
+        if i!="4" and i!="7":
+            flag = False
+            n -= 1
+    if flag :
+        print(n)
+        break
