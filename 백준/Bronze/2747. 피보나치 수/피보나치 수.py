4afa79974a72ff44bd4f611e8d0a036a9f0d99b3
서선아 <108112575+seon03@users.noreply.github.com>
2025-04-17 17:08:02 +0900

    [Bronze II] Title: 피보나치 수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.py
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.py
@@ -2,9 +2,17 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
+dp = [0] * (N+1) 
 
-dp = [0] * (N + 1)
-dp[1] = 1
-for i in range(2, N+1):
-  dp[i] = dp[i-1] + dp[i-2]
-print(dp[N])
+if N == 1:
+  dp[1] = 1
+  print(dp[1])
+elif N == 2:
+  dp[2] = 1
+  print(dp[2])
+else:
+  dp[1] = 1
+  dp[2] = 1
+  for i in range(3, N+1):
+    dp[i] = dp[i-1] + dp[i-2]
+  print(dp[N])
